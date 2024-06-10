add832029745f1977e9acdb590c8589bef4cfc02
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-10 17:28:25 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/login",
+//     element: <LoginPage></LoginPage>,
+//   },
+//   {
+//     path: "/signup",
+//     element: <SignupPage></SignupPage>,
+//   },
+//   {
+//     path: "/",
+//     element: <Protected><Home></Home></Protected>,
+//   },
+//   {
+//     path: "/admin",
+//     element: <ProtectedAdmin><AdminHome></AdminHome></ProtectedAdmin>,
+//   },
+//   {
+//     path: "/cart",
+//     element: <Protected><CartPage></CartPage></Protected>,
+//   },
+//   {
+//     path: "/checkout",
+//     element: <Protected><Checkout></Checkout></Protected>,
+//   },
+//   {
+//     path: "/product-detail/:id",
+//     element: <Protected><ProductDetailPage></ProductDetailPage></Protected>,
+//   },
+//   {
+//     path: "/admin/product-detail/:id",
+//     element: <ProtectedAdmin><AdminProductDetailPage></AdminProductDetailPage></ProtectedAdmin>,
+//   },
+//   {
+//     path: "/admin/product-form",
+//     element: <ProtectedAdmin><AdminProductFormPage></AdminProductFormPage></ProtectedAdmin>,
+//   },
+//   {
+//     path: "/admin/orders",
+//     element: <ProtectedAdmin><AdminOrderspage></AdminOrderspage></ProtectedAdmin>,
+//   },
+//   {
+//     path: "/admin/product-form/edit/:id",
+//     element: <ProtectedAdmin><AdminProductFormPage></AdminProductFormPage></ProtectedAdmin>,
+//   },
+//   {
+//     path: "/order-success/:id",
+//     element: (
+//       <Protected>
+//         <OrderSucessPage></OrderSucessPage>{' '}
+//       </Protected>
+//     ),
+//   },
+//   {
+//     path: "/my-orders",
+//     element: (
+//       <Protected>
+//         <UsersOrderPage></UsersOrderPage>{' '}
+//       </Protected>
+//     ),
+//   },
+//   {
+//     path: "/profile",
+//     element: (
+//       <Protected>
+//         <UsersProfilePage></UsersProfilePage>{' '}
+//       </Protected>
+//     ),
+//   },
+//   {
+//     path: "/stripe-checkout/",
+//     element: (
+//       <Protected>
+//         <StripeCheckout></StripeCheckout>
+//       </Protected>
+//     ),
+//   },
+//   {
+//     path: "/logout",
+//     element: <Logout></Logout>,
+//   },
+//   {
+//     path: "/forgot-password",
+//     element: <ForgotPasswordPage></ForgotPasswordPage>,
+//   },
+//   {
+//     path: "/reset-password",
+//     element: <ResetPasswordPage></ResetPasswordPage>,
+//   },
+//   {
+//     path: "*",
+//     element: <PageNotFound></PageNotFound>,
+//   },
+// ]);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
     </div>
   );
 }
