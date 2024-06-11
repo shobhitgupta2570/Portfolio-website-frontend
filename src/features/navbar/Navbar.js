1e8945e4a18dcac3544adf1e4b6d9b772fe21e0a
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-11 18:33:08 +0530

    navbar design

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -13,12 +13,16 @@ export default function Navbar() {
  
   
   return (
+    <div className="h-[100%] w-[100%]">
     <div className="w-[100%] bg-black-400 h-[50px] flex items-center justify-end pr-[5%] bg-green-500">
       
       <div className=" flex items-end">
       <GiHamburgerMenu size="30" />
       </div>
-      <div className=""></div>
+    </div>
+    <div className="flex justify-end h-[60px] w-[60%] bg-blue-200">
+    <div className="  h-[60%] w-[40%] bg-red-400 "></div>
+    </div>
     </div>
   );
 }
