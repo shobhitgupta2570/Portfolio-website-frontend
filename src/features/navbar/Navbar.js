b2662874c9c1e09b157d246b08a373df9c1cf0a0
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-14 17:12:11 +0530

    Responsiveness

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
  
   
   return (
-    <div className="h-[100%] w-[100%]">
+    <div onClick={(e)=>{setMenu(!menu)}} className="h-[100%] w-[100%]">
     <div className="w-[100%] bg-black-400 h-[50px] flex items-center justify-end pr-[5%] bg-green-500">
       
       <div onClick={(e)=>{setMenu(!menu)}}  className=" flex items-end pl-2">
@@ -22,16 +22,17 @@ export default function Navbar() {
       </div>
     </div>
     {menu?
-      <div className="relative w-[100%] px-[5%] bg-gray-100">
-      <div className=" bg-red-300 w-[140px]  lg:mr-7 flex flex-col items-center  absolute right-0 py-2">
-      <div><button className="text-xl">Experience</button></div>
-      <div><button className="text-xl">Projects</button></div>
-      <div><button className="text-xl">About</button></div>
-      <div><button className="text-xl">Contact Us</button></div>
-      <div><button className="text-xl">Education</button></div>
+      <div className="relative w-[100%] px-[5%]">
+      <div className=" bg-gray-50 w-[140px]  lg:mr-7 flex flex-col items-center  absolute right-0 py-2">
+      <div><button className="text-xl shadow-md shadow-grey">Experience</button></div>
+      <div><button className="text-xl shadow-md shadow-grey">Projects</button></div>
+      <div><button className="text-xl shadow-md shadow-grey">About</button></div>
+      <div><button className="text-xl shadow-md shadow-grey">Contact Us</button></div>
+      <div><button className="text-xl shadow-md shadow-grey">Education</button></div>
       </div>
     </div>
     : ""}
+    
     </div>
   );
 }
