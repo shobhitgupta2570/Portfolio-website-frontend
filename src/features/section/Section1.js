8b3d106d7de5017b873c27c56d80d253c3cfbdd3
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-14 17:24:14 +0530

    changes

---
+++ src/features/section/Section1.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+// import { useSelector, useDispatch } from 'react-redux';
+// import {
+//   increment,
+//   incrementAsync,
+//   selectCount,
+// } from './navbarSlice';
+
+export default function Navbar() {
+  // const count = useSelector(selectCount);
+  // const dispatch = useDispatch();
+  const [menu , setMenu] = useState(false);
+ 
+  
+  return (
+    <div className="h-[500px] w-[100%] bg-blue-200">
+   
+    
+    </div>
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "../features/navbar/Navbar"
+import Section1 from "../features/section/Section1"
 
 export default function HomePage() {
   return (
   <>
     <Navbar></Navbar>
+    <Section1></Section1>
     
     </>
   )
