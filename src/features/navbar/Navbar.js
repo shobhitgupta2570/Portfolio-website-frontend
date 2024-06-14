a2c4fc4de118fe51cc5e2d93f4e9660e3f34bb4d
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-14 16:50:21 +0530

    menu changes

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -14,23 +14,24 @@ export default function Navbar() {
  
   
   return (
-    <div className="h-[100%] w-[100%] flex">
+    <div className="h-[100%] w-[100%]">
     <div className="w-[100%] bg-black-400 h-[50px] flex items-center justify-end pr-[5%] bg-green-500">
       
-      <div onClick={(e)=>{setMenu(!menu)}}  className=" flex items-end">
+      <div onClick={(e)=>{setMenu(!menu)}}  className=" flex items-end pl-2">
       <GiHamburgerMenu size="30" />
       </div>
     </div>
-    {/* {menu? */}
-      <div className="flex items-center justify-end w-[150px] px-[5%] bg-gray-100">
-        
+    {menu?
+      <div className="relative w-[100%] px-[5%] bg-gray-100">
+      <div className=" bg-red-300 w-[140px]  lg:mr-7 flex flex-col items-center  absolute right-0 py-2">
       <div><button className="text-xl">Experience</button></div>
       <div><button className="text-xl">Projects</button></div>
       <div><button className="text-xl">About</button></div>
       <div><button className="text-xl">Contact Us</button></div>
       <div><button className="text-xl">Education</button></div>
+      </div>
     </div>
-    {/* // : ""} */}
+    : ""}
     </div>
   );
 }
