6ea2b185a354fdf9e3671e90f6abd9435f9b2c75
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-20 17:20:18 +0530

    mui integration

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from "../features/navbar/Navbar"
+import Navbar1 from "../features/navbar/Navbar1"
 import Section1 from "../features/section/Section1"
 
 export default function HomePage() {
   return (
   <>
-    <Navbar></Navbar>
-    <Section1></Section1>
-    
+    {/* <Navbar></Navbar>
+    <Section1></Section1> */}
+    <Navbar1></Navbar1>
     </>
   )
 }
